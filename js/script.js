0b3e4f78b13c1eb3d92e04a95d3a495e2b772db5
Guilherme Luiz Quintino Da Silva <32807543+Guilherme1991@users.noreply.github.com>
2019-06-14 12:53:28 -0300

    New config in Layout
    New config in Layout

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let form = document.getElementById('form'),
+       let form = document.getElementById('form'),
        name = document.getElementById('nome'),
        job = document.getElementById('job'),
        btn = document.querySelector('.submit')
